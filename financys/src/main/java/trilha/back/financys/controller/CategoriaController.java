560fd305b9c3b2196405aa7f23a78db5aa36d8fb
SonCarreiro1910 <81304054+SonCarreiro1910@users.noreply.github.com>
2022-02-10 19:10:03 -0300

    Inclusão do Desafio 3 - Codificação

---
+++ financys/src/main/java/trilha/back/financys/controller/CategoriaController.java
@@ -0,0 +1,38 @@
+package trilha.back.financys.controller;
+
+import io.swagger.annotations.Api;
+//import io.swagger.annotations.ApiOperation;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+import trilha.back.financys.models.Categoria;
+import java.util.ArrayList;
+import java.util.List;
+
+
+//@RestController
+//@ResponseBody
+//@RequestMapping(value = "/categoria")
+//@Api(value = "FinancysApplication")
+//@CrossOrigin(origins = "*") //Liberar qualquer dominio para acessar *libera todos
+public class CategoriaController {
+    public ArrayList<Categoria> lista = new ArrayList<Categoria>();
+
+
+    //@GetMapping("/ler") //categoria
+    //@ApiOperation(value = "Retorna todas categorias")
+    public ResponseEntity<List<Categoria>> categoriaLista() {
+        return ResponseEntity.ok(lista);
+
+    }
+    //@PostMapping("/criar") //categoria
+    //@ApiOperation(value = "Cria uma categoria")
+    public ResponseEntity<Categoria> categoriaSalva(@RequestBody Categoria categoriaBody) {
+        var categoria = new Categoria();
+        categoria.setId(categoriaBody.getId());
+        categoria.setName(categoriaBody.getName());
+        categoria.setDescription(categoriaBody.getDescription());
+        lista.add(categoria);
+        return ResponseEntity.ok(categoria);
+    }
+
+}
